635be19e667f00cc890f1d5d6f247365538a9762
Yoga Willy <yogawilly@outlook.com>
2026-09-10 03:30:11 +0700

    feat(trust): centralize public BP identity

---
+++ src/config/site.js
@@ -0,0 +1,8 @@
+export const SITE = {
+  name: "Bakaran Project",
+  url: "https://bakaranproject.com",
+  email: "bakaranproject@gmail.com",
+  social: {
+    github: "https://github.com/Bakaran-Project",
+  },
+};
