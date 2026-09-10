f29ff9f853a3249ea42f6f7543342084b3c02e9f
Yoga Willy <yogawilly@outlook.com>
2026-09-10 08:49:36 +0700

    feat(m5): establish main landmark

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,11 +9,15 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <HeroSection />
-      <ServicesSection />
-      <PortfolioSection/>
-      <ContactSection />
-      <Footer/>
+
+      <main id="main-content">
+        <HeroSection />
+        <ServicesSection />
+        <PortfolioSection />
+        <ContactSection />
+      </main>
+
+      <Footer />
     </>
   );
 }
