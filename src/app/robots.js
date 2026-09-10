13b4ce4b37f55bc82085c6737b01f72b1062039e
Yoga Willy <yogawilly@outlook.com>
2026-09-10 14:40:14 +0700

    feat(m6): add robots metadata route

---
+++ src/app/robots.js
@@ -0,0 +1,12 @@
+import { SITE } from "@/config/site";
+
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: `${SITE.url}/sitemap.xml`,
+    host: SITE.url,
+  };
+}
