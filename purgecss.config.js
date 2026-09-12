46f342e4c91bb921cb5fe2e69100317deb42e940
Yoga Willy <yogawilly@outlook.com>
2026-09-12 19:40:04 +0700

    chore(m7): remove legacy purgecss config

--- purgecss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    content: [
-        "./app/**/*.{js,jsx,ts,tsx}",
-        "./components/**/*.{js,jsx,ts,tsx}",
-        "./features/**/*.{js,jsx,ts,tsx}"
-    ],
-    css: ["./app/globals.css"],
-    output: "./purged-css"
-};
