d88de79878c740873f263c23838e6edd368726c5
Yoga Willy <yogawilly@outlook.com>
2026-09-12 22:21:04 +0700

    docs(m8): type portfolio filter tags

--- src/features/portfolio/portfolio.types.js
+++ src/features/portfolio/portfolio.types.js
@@ -3,7 +3,10 @@
  * @property {string} code
  * @property {string} title
  * @property {string} category
+ * @property {string[]} filterTags
+ * @property {string} status
+ * @property {string} statusDetail
  * @property {string[]} tech
  * @property {string} description
- * @property {string[]} images
+ * @property {string[]} proof
  */
